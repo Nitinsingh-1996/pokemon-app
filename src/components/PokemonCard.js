2d1f0a33ceeed08394ce7e2a01e854531fc15ddb
Nitin singh <nitin.singh@hestabit.in>
2024-10-16 12:23:07 +0530

    code done

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,10 +1,13 @@
-
+import PropTypes from "prop-types";
 
 const PokemonCard = ({ pokemon }) => (
-  <div className="card p-4 shadow-md">
+  <div className="card p-4 shadow-md bg-slate-100 flex flex-col justify-center items-center gap-4">
     <img src={pokemon.image} alt={pokemon.name} className="w-20 h-20" />
-    <p>{pokemon.name}</p>
+    <p className="font-semibold">{pokemon.name}</p>
   </div>
 );
 
 export default PokemonCard;
+PokemonCard.propTypes = {
+  pokemon: PropTypes.object,
+};
--- src/pages/pokemon/[name].js
+++ src/pages/pokemon/[name].js
@@ -1,7 +1,18 @@
+import { Close } from "@/assets/iconSVG";
 import Image from "next/image";
 import Link from "next/link";
+import PropTypes from "prop-types";
+import { useState } from "react";
 
 export default function PokemonDetails({ pokemon }) {
+  const [showModal, setShowModal] = useState(false);
+  const handleShowAll = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
   return (
     <div className="p-4">
       <div className="mb-4">
@@ -18,18 +29,71 @@ export default function PokemonDetails({ pokemon }) {
           <Image src={pokemon.sprite} alt={pokemon.name} layout="fill" />
         </div>
         <div className="bg-yellow-900 p-4 text-white">
-          <p className="mb-2 last:mb-0">
-            <b>Name:</b> {pokemon.name}
+          <p className="mb-2 last:mb-0 flex gap-x-1">
+            {console.log(pokemon.allData, "allData")}
+            {console.log(pokemon.stat, "allData stat")}
+            <b className="min-w-[70px]">Name:</b> {pokemon.name}
           </p>
-          <p className="mb-2 last:mb-0">
-            <b>Type:</b> {pokemon.types.join(", ")}
+          <p className="mb-2 last:mb-0 flex gap-x-1">
+            <b className="min-w-[70px]">Type:</b> {pokemon.types.join(", ")}
           </p>
-          <p className="mb-2 last:mb-0">
-            <b>Height:</b> {pokemon.height}
-          </p>
-          <p className="mb-2 last:mb-0">
-            <b>Weight:</b> {pokemon.weight}
-          </p>
+          <div className="mb-2 last:mb-0 flex gap-x-1">
+            <b className="min-w-[70px]">stat:</b>{" "}
+            <div className="flex gap-1 justify-start flex-wrap">
+              {pokemon.stat.map((items, index) => (
+                <>
+                  <span className="whitespace-nowrap" key={index + 1}>
+                    {items.stat.name}
+                  </span>
+                  <span className="last:hidden">,</span>
+                </>
+              ))}
+            </div>
+          </div>
+          <div className="mb-2 last:mb-0 flex gap-x-1">
+            <b className="min-w-[70px]">abilities:</b>{" "}
+            <div className="flex gap-1 justify-start flex-wrap">
+              {pokemon.abilities.map((items, index) => (
+                <>
+                  <span className="whitespace-nowrap" key={index + 1}>
+                    {items.ability.name}
+                  </span>
+                  <span className="last:hidden">,</span>
+                </>
+              ))}
+            </div>
+          </div>
+          <div className="mb-2 last:mb-0 flex gap-x-1">
+            <b className="min-w-[70px]">moves:</b>{" "}
+            <div className="flex gap-1 justify-start flex-wrap">
+              {pokemon.moves.slice(0, 5).map((items, index) => (
+                <>
+                  <span className="whitespace-nowrap" key={index + 1}>
+                    {items.move.name}
+                  </span>
+                  <span className="last:hidden">,</span>
+                </>
+              ))}
+              {pokemon.moves.length > 5 && (
+                <button onClick={handleShowAll} className="bg-white px-2 text-black hover:bg-black hover:text-white transition-all">...</button>
+              )}
+              {showModal && (
+                <div className="fixed top-0 left-0 right-0 bottom-0 max-h-[400px] w-[540px] m-auto bg-slate-400 rounded-xl p-6 pt-14 text-black">
+                  <div className="overflow-auto h-full">
+                    <h3 className="absolute top-3 left-3 font-semibold">All Map Values</h3>
+                    <ul>
+                      {pokemon.moves.map((items, index) => (
+                        <li key={index + 1} className="bg-slate-100 p-2 mb-1">{items.move.name}</li>
+                      ))}
+                    </ul>
+                    <button className="absolute right-3 top-3" onClick={handleCloseModal}>
+                      <Close/>
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -45,6 +109,14 @@ export async function getServerSideProps({ params }) {
     types: data.types.map((t) => t.type.name),
     height: data.height,
     weight: data.weight,
+    allData: data,
+    stat: data.stats,
+    abilities: data.abilities,
+    moves: data.moves,
   };
   return { props: { pokemon } };
 }
+
+PokemonDetails.propTypes = {
+  pokemon: PropTypes.object,
+};
